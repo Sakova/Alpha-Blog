eabb3ffb618396642111b556a9f7cdfe797deab3
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-07-19 15:34:59 +0300

    fix articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_121821) do
+ActiveRecord::Schema.define(version: 2022_07_19_122937) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
-    t.datetime "update_at"
+    t.datetime "updated_at"
   end
 
 end
