e25c6a96686fbb960d902c54bafdba711774880f
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-07-21 09:10:28 +0300

    some changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,11 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("@popperjs/core")
+
+import "bootstrap"
+
+// Import the specific modules you may need (Modal, Alert, etc)
+import { Tooltip, Popover } from "bootstrap"
+
