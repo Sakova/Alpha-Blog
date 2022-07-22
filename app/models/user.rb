dbd8579ca6c6e5f91d64e260a0b0d2f31b9b8cf6
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-07-22 18:51:19 +0300

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
 
+class User < ApplicationRecord
+  validates :username, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
